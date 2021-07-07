f1371635726a762a08cfa1d2e1c1a218005acfb3
Fatma <fceyda@gmail.com>
2021-07-07 13:57:06 -0700

    create controller and from model

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,22 +6,51 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using PowerPointGenerator.Models;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Configuration;
 
 namespace PowerPointGenerator.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private static string _key = ConfigurationManager.AppSettings.Get("Key");
+        private static string _baseUri = "https://api.bing.microsoft.com/v7.0/images/search";
+        private const string QUERY_PARAMETER = "?q="; 
+        private const string MKT_PARAMETER = "&mkt="; 
+        private static string _clientIdHeader = null;
+
+        public async Task<ActionResult> Index()
+        {
+            FormModel newModel = new FormModel();
+            //add search string instead of the example below with instance of form model search string
+            var searchString = "cat";
+            var client = new HttpClient();
+            var queryString = QUERY_PARAMETER + Uri.EscapeDataString(searchString); 
+            queryString += MKT_PARAMETER + "en-us";
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", _key);
+            HttpResponseMessage response = await client.GetAsync(_baseUri + queryString);
+            var contentString = await response.Content.ReadAsStringAsync();
+                Dictionary<string, object> searchResponse = JsonConvert.DeserializeObject<Dictionary<string, object>>(contentString);
+            if(response.IsSuccessStatusCode)
+            {
+                newModel.PrintImages(searchResponse);
+            }
+
+            return View();
+        }
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+        // public IActionResult Index()
+        // {
+        //     return View();
+        // }
 
         public IActionResult Privacy()
         {
---
+++ Models/FormModel.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.Configuration;
+
+namespace PowerPointGenerator.Models
+{
+  public class FormModel
+  {
+    public string Title { get; set; }
+    public string Content { get; set; }
+    private string searchString {get; set;}
+    public List<string> SearchWords = new List<string> {};
+
+     public void TitleToSearcList()
+    {
+      // Title = title;
+      char[] separators = new char[] { ' ', '.' };
+      string[] subs = Title.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+      foreach (var sub in subs)
+      {
+        SearchWords.Add(sub);
+      }
+      searchString = string.Join(", ",SearchWords);
+      Console.WriteLine(searchString);
+    }
+
+    public void PrintImages(Dictionary<string, object> response)
+        {
+            Console.WriteLine("The response contains the following images:\n");
+            var images = response["value"] as Newtonsoft.Json.Linq.JToken;
+            foreach (Newtonsoft.Json.Linq.JToken image in images)
+            {
+                Console.WriteLine("Thumbnail: " + image["thumbnailUrl"]);
+                Console.WriteLine("Thumbnail size: {0} (w) x {1} (h) ", image["thumbnail"]["width"], image["thumbnail"]["height"]);
+                Console.WriteLine("Original image: " + image["contentUrl"]);
+                Console.WriteLine("Original image size: {0} (w) x {1} (h) ", image["width"], image["height"]);
+                Console.WriteLine("Host: {0} ({1})", image["hostPageDomainFriendlyName"], image["hostPageDisplayUrl"]);
+                Console.WriteLine();
+            }
+        }
+
+      // static void ImagesFromAPI(Dictionary<string, object> response)
+      //   {
+      //     var images = response["value"] as Newtonsoft.Json.Linq.JToken;
+      //     foreach (Newtonsoft.Json.Linq.JToken image in images)
+      //       {
+      //         string thumbnailUrl = image["thumbnailUrl"].ToString();
+      //         string imageUrl = image["contentUrl"].ToString();
+      //         ImageModel newImage = new ImageModel(thumbnailUrl, imageUrl);
+      //         FormModel nModel = new FormModel();
+      //         nModel.nImageList.Add(newImage);
+      //         ImageList.Add(newImage);
+      //         string img = image["thumbnailUrl"].ToString();
+      //         thumbnails.Add(img);
+      //       }
+      //   }
+
+
+  }
+}
